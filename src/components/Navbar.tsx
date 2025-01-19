102f5f3b507ec81239413c4c3d5fc8c0c901d602
Aleem Mohammed <aleem.btech@gmail.com>
2025-01-19 09:49:36 +0300

    Updated verbiage for create new account

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -126,7 +126,7 @@ const Navbar: React.FC = () => {
                       className="block w-full text-left px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
                       onClick={() => setIsMenuOpen(false)}
                     >
-                      Register
+                      Create Account
                     </Link>
                   )}
                   <button
